602e5b8e8edb3048c44bc5fb13954946401e37a9
toanilsharma <toanilsharma@gmail.com>
2025-10-12 20:41:40 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // 👇 THIS IS THE LINE YOU NEED TO ADD
+      base: '/',
+
       server: {
         port: 3000,
         host: '0.0.0.0',
